089394ce71d0e8aafaf111673909c7602d01862b
Krunalsinh27 <parmarkrunalsinh37@gmail.com>
2026-05-30 22:20:53 +0530

    Add admin panel

---
+++ admin/dashboard.php
@@ -0,0 +1,36 @@
+<?php
+require('inc/essentials.php');
+adminLogin();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Panel - Dashboard</title>
+    <?php require('inc/links.php'); ?>
+</head>
+
+<body class="bg-light">
+
+    <?php require('inc/header.php'); ?>
+
+    <div class="container-fluid" id="main-content">
+        <div class="row">
+            <div class="col-lg-10 ms-auto p-4 overflow-hidden">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus deserunt dolores odio quidem voluptatem, esse iusto fugit ratione iure nostrum! Laudantium distinctio consectetur quo tempore sunt, laborum non accusamus et.
+                Omnis nobis laboriosam voluptatem delectus. Placeat asperiores eius libero incidunt. Ullam corporis vitae porro nostrum mollitia minus delectus beatae quod dolorem, consectetur vero praesentium et, repudiandae dolor quo officiis inventore!
+                Minus ad eveniet aliquid nostrum unde molestias modi temporibus culpa assumenda velit ex, optio iure repellat totam maxime perspiciatis ipsum excepturi voluptatibus esse quae eos. Impedit adipisci inventore modi alias?
+                Totam cumque vero ratione architecto, error ad aut possimus reprehenderit, voluptatibus iusto, necessitatibus iure itaque neque atque nobis repellat! Non quibusdam eum dicta modi accusamus! Odit ad labore velit excepturi.
+                Nisi, minima adipisci odio, non tenetur officiis dolorum animi illo praesentium fugiat aperiam voluptate distinctio sed minus eius porro ullam accusamus vitae. Optio esse alias molestiae, ipsa adipisci dolor eum.
+                
+            </div>
+        </div>
+    </div>
+
+
+    <?php require('inc/scripts.php'); ?>
+</body>
+
+</html>
